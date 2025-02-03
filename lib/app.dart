ec1ce6b5afcf522d906c1bef412c6537047ff074
white-king1 <kelechivctorn@gmail.com>
2025-02-03 16:12:32 -0500

    added an app.dart page

--- lib/app.dart
+++ lib/app.dart
@@ -3,16 +3,6 @@
 import 'package:firebase_shop_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  // Todo: Add widgets Binding
-  // Todo: Init Local Storage
-  // Todo: Await Native Splash
-  // Todo: Initialise Firebase
-  // Todo: Initialize Authenticaton
-
-  runApp(const App());
-}
-
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -22,6 +12,9 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
+      home: Scaffold(
+        body: Center(child: Text("Hello, World!")),
+      ),
     );
   }
 }
