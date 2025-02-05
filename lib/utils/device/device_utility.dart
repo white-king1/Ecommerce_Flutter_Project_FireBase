fb199fcfeb832a8597f69c8dfe90b10190ab2c98
white-king1 <kelechivctorn@gmail.com>
2025-02-05 06:52:59 -0500

    corrected device_utility.dart page

--- lib/utils/device/device_utility.dart
+++ lib/utils/device/device_utility.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class TDeviceUtils {
-  static void hideKeyboard(BuildContext context){
+  static void hideKeyboard(BuildContext context) {
     FocusScope.of(context).requestFocus(FocusNode());
   }
 
@@ -16,65 +16,63 @@ class TDeviceUtils {
     );
   }
 
-  static bool isLandscopeOrientation(BuildContext context) {
-    final viewInsets = View.of(context).viewInsets;
-    return viewInsets.bottom == 0;
+  static bool isLandscapeOrientation(BuildContext context) {
+    return MediaQuery.of(context).orientation == Orientation.landscape;
   }
 
   static bool isPortraitOrientation(BuildContext context) {
-    final viewInsets = View.of(context).viewInsets;
-    return viewInsets.bottom != 0;
+    return MediaQuery.of(context).orientation == Orientation.portrait;
   }
 
   static void setFullScreen(bool enable) {
-    SystemChrome.setEnabledSystemUIMode(enable ?SystemUiMode.immersiveSticky : SystemUiMode.edgeToEdge);
+    SystemChrome.setEnabledSystemUIMode(
+      enable ? SystemUiMode.immersiveSticky : SystemUiMode.edgeToEdge,
+    );
   }
 
-  static double getScreenHeight() {
-    return MediaQuery.of(Get.context!).size.height;
+  static double getScreenHeight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
   }
 
-   static double getScreenWidth(BuildContext context) {
+  static double getScreenWidth(BuildContext context) {
     return MediaQuery.of(context).size.width;
   }
 
-   static double getPixelRatio() {
-    return MediaQuery.of(Get.context!).devicePixelRatio;
+  static double getPixelRatio(BuildContext context) {
+    return MediaQuery.of(context).devicePixelRatio;
   }
 
-  static double getStatusBarHeight() {
-    return MediaQuery.of(Get.context!).padding.top;
+  static double getStatusBarHeight(BuildContext context) {
+    return MediaQuery.of(context).padding.top;
   }
 
-   static double getBottomNavigationBarHeight() {
+  static double getBottomNavigationBarHeight() {
     return kBottomNavigationBarHeight;
   }
-static double getAppBarHeight() {
+
+  static double getAppBarHeight() {
     return kToolbarHeight;
   }
 
-  static double getKeyboardHeight() async {
-    final viewInsets = MediaQuery.of(Get.context!).viewInsets;
-    return viewInsets.bottom;
+  static double getKeyboardHeight(BuildContext context) {
+    return MediaQuery.of(context).viewInsets.bottom;
   }
 
-
- static Future<bool> isKeyboardVisible() async {
-    final viewInsets = View.of(Get.context!).viewInsets;
-    return viewInsets.bottom > 0;
+  static bool isKeyboardVisible(BuildContext context) {
+    return MediaQuery.of(context).viewInsets.bottom > 0;
   }
 
   static Future<bool> isPhysicalDevice() async {
     return defaultTargetPlatform == TargetPlatform.android || defaultTargetPlatform == TargetPlatform.iOS;
   }
 
-static void vibrate(Duration duration){
-  HapticFeedback.vibrate();
-  Future.delayed(duration, () => HapticFeedback.vibrate());
-}
+  static void vibrate(Duration duration) {
+    HapticFeedback.vibrate();
+    Future.delayed(duration, () => HapticFeedback.vibrate());
+  }
 
-static Future<bool> setPreferredOrientations(List<DeviceOrientation> orientations) async {
-   await SystemChrome.setPreferredOrientations(orientations);
+  static Future<void> setPreferredOrientations(List<DeviceOrientation> orientations) async {
+    await SystemChrome.setPreferredOrientations(orientations);
   }
 
   static void hideStatusBar() {
@@ -82,33 +80,31 @@ static Future<bool> setPreferredOrientations(List<DeviceOrientation> orientation
   }
 
   static void showStatusBar() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverly.values)
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
   }
-
 
   static Future<bool> hasInternetConnection() async {
-  try {
-    final result = await InternetAddress.lookup('example.com');
-    return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
-
-  } on SocketException catch (_) {
-    return false;
-  }
+    try {
+      final result = await InternetAddress.lookup('example.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
   }
 
   static bool isIOS() {
     return Platform.isIOS;
   }
 
-   static bool isAndroid() {
+  static bool isAndroid() {
     return Platform.isAndroid;
   }
 
   static void launchUrl(String url) async {
-    if(await canLaunchUrlString(url)){
+    if (await canLaunchUrlString(url)) {
       await launchUrlString(url);
-    }else{
-      throw 'Could not launch Url';
+    } else {
+      throw 'Could not launch URL';
     }
   }
 }
